5af82ebbfc5850e4a9842dd62261576867b1a144
Katarina Brayner <katarinabrayner@gmail.com>
2023-02-21 20:53:42 -0300

    Add CRUD for Publishers

--- Controllers/PublisherController.cs
+++ Controllers/PublisherController.cs
@@ -1,5 +1,7 @@
-﻿using BookstoreSystem.Models;
-using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using BookstoreSystem.DTOs;
+using BookstoreSystem.Models;
+using BookstoreSystem.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookstoreSystem.Controllers
@@ -8,15 +10,74 @@ namespace BookstoreSystem.Controllers
     [ApiController]
     public class PublisherController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Book>> GetAll()
+        private readonly IPublisherRepository _publisherRepository;
+        private readonly IMapper _mapper;
+        public PublisherController(IPublisherRepository publisherRepository, IMapper mapper)
         {
-            return Ok();
+            _mapper = mapper;
+            _publisherRepository = publisherRepository;
         }
-/*        [HttpGet]
-        public ActionResult<List<Book>> GetByTitle(string title)
+
+        [HttpGet]
+        public async Task<ActionResult<List<PublisherDTO>>> GetPublishers(string? name)
         {
-            return Ok();
-        }*/
+            List<Publisher> publishers;
+            if (name == null)
+            {
+                publishers = await _publisherRepository.GetAll();
+            }
+            else
+            {
+                publishers = await _publisherRepository.GetByName(name);
+            }
+
+            return Ok(_mapper.Map<IEnumerable<PublisherDTO>>(publishers));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PublisherDTO>> GetById(int id)
+        {
+            Publisher publisher = await _publisherRepository.GetById(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<PublisherDTO>(publisher));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PublisherDTO>> Add([FromBody] PublisherDTO publisherDTO)
+        {
+            Publisher publisher = _mapper.Map<Publisher>(publisherDTO);
+
+            Publisher savedPublisher = await _publisherRepository.Add(publisher);
+
+            return Ok(_mapper.Map<PublisherDTO>(savedPublisher));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PublisherDTO>> Update([FromBody] PublisherDTO publisherDTO, int id)
+        {
+            publisherDTO.Id = id;
+            Publisher publisher = _mapper.Map<Publisher>(publisherDTO);
+
+            bool foundId = _publisherRepository.IdExists(id);
+
+            if (!foundId)
+            {
+                return NotFound();
+            }
+
+            Publisher savedPublisher = await _publisherRepository.Add(publisher);
+
+            return Ok(_mapper.Map<PublisherDTO>(savedPublisher));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<int>> DeleteById(int id)
+        {
+            int result = await _publisherRepository.Delete(id);
+            return Ok(result);
+        }
     }
 }
