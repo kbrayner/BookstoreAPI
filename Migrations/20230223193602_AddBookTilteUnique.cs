283acf0c84a929c87dc7ea82f0204b43617343dd
Katarina Brayner <katarinabrayner@gmail.com>
2023-02-23 17:13:51 -0300

    Add migration to create unique constraint to book title

---
+++ Migrations/20230223193602_AddBookTilteUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookstoreSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookTilteUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Title",
+                table: "Books",
+                column: "Title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Books_Title",
+                table: "Books");
+        }
+    }
+}
